6cd733133965929d4a279f43745e230203b2efc1
Cody Traylor <ct3yl0vg5n@users.noreply.github.com>
2026-04-19 11:44:25 +0800

    Add clarifying comment about exit code 2 behavior in CI vs local

--- cmd/syft/main.go
+++ cmd/syft/main.go
@@ -17,6 +17,7 @@ func main() {
 	if err := cli.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		// use exit code 2 to distinguish application errors from OS-level errors (exit code 1)
+		// note: some CI systems treat any non-zero exit code the same, but this helps with local debugging
 		os.Exit(2)
 	}
 }
